e8abd6e03f3af05ecfab24a8f1a28fe8d7287c30
Thomas Citharel <tcit@tcit.fr>
2017-03-16 14:22:55 +0100

    Add ant

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -12,7 +12,7 @@ curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 # Install other requirements
 
 apt-get update -yqq
-apt-get install git wget libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev nodejs build-essential bzip2 -yqq
+apt-get install git wget libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev nodejs build-essential bzip2 ant -yqq
 
 npm i
 npm i grunt-cli
