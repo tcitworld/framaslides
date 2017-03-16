1dda75534feee89439c9c9edda43654c04d4ca60
Thomas Citharel <tcit@tcit.fr>
2017-03-16 14:53:04 +0100

    Fix #30

--- app/locales/fr.js
+++ app/locales/fr.js
@@ -39,6 +39,7 @@ define({
   preview: 'Aperçu',
   upload: 'Téléverser',
   video_info: 'Supporte <strong>webm & YouTube</strong>.<br/>Essayez par exemple&nbsp;: http://www.youtube.com/watch?v=vHUsdkmr-SM',
+  video_doc: 'Lire <a href="https://docs.framasoft.org/fr/strut/videos.html#d%C3%A9placer-une-vid%C3%A9o" target="_blank">la documentation</a> pour savoir comment déplacer les vidéos',
   picture_info: "Cliquez sur <strong>\"Téléverser\"</strong> pour choisir une image sur votre ordinateur, ou <strong>copiez-collez</strong> directement l'adresse d'une image en ligne.",
   website_info: "<strong>Attention</strong>, un certain nombre de sites web refusent d'être intégrés ainsi. Il est donc possible que vous n'obteniez rien.",
   saveWarning: 'Vous devez enregistrer la présentation avant de pouvoir la visionner.\nPour effectuer cela, rendez-vous dans le menu principal en cliquant sur le bouton en haut à gauche.',
