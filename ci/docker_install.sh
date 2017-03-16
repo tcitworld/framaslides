c741a3f7928c8a70cd7f8c51457c2b798bab3e08
Thomas Citharel <tcit@tcit.fr>
2017-03-16 14:07:17 +0100

    Cache node_modules and fix grunt

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -15,8 +15,9 @@ apt-get update -yqq
 apt-get install git wget libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev nodejs build-essential bzip2 -yqq
 
 npm i
+npm i grunt-cli
 
-grunt build
+./node_modules/grunt-cli/bin/grunt build
 
 # Compile PHP, include these extensions.
 docker-php-ext-install mbstring mcrypt pdo_sqlite curl json intl gd xml zip bz2 opcache
